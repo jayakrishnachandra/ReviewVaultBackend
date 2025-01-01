0c6f3cdcedc948f00fb36f20ff55e5bf55c632ac
Vemana Jaya Krishna Chandra <vemanajayakrishnachandra@gmail.com>
2025-01-01 21:45:16 +0530

    added port

---
+++ src/main/java/dev/jay/MovieService.java
@@ -0,0 +1,22 @@
+package dev.jay;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
+    }
+}
